b00490565c9ee45c2c7b46a2c98d7214b4cae544
Aaron Eppert <aaron.eppert@packetsled.com>
2016-11-22 13:50:10 -0500

    Start of making sure we have a Go compiler installed for ARM also

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-: ${RPXC_IMAGE:=sdthirlwall/raspberry-pi-cross-compiler}
+GO_URL=https://storage.googleapis.com/golang
+GO_VER=go1.7.3.linux-armv6l.tar.gz
 
+install_go() {
+	GO_DEST=image/usr/local
+	if [ ! -f "$GO_VER" ]; then
+		curl -O -J $GO_URL/$GO_VER
+	fi
+
+	mkdir -p $GO_DEST	
+	tar xvfj $GO_VER -C $GO_DEST
+}
+
+: ${RPXC_IMAGE:=aeppert/raspberry-pi-cross-compiler}
+
+install_go
 docker build -t $RPXC_IMAGE .
